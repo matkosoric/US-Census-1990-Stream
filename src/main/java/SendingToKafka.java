796a93eb8c388f5c0043cb74569b32ec8421922c
matko-CT <soric.matko@gmail.com>
2019-01-28 13:41:34 +0100

    topic forking by gender 0 -> male 1 -> female

---
+++ src/main/java/SendingToKafka.java
@@ -0,0 +1,73 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+public class SendingToKafka {
+
+    private final static String TOPIC_MALE = "us-census-male";
+    private final static String TOPIC_FEMALE = "us-census-female";
+
+    private static final Logger LOGGER = Logger.getLogger(SendingToKafka.class.getName());
+
+
+    public static void main(String[] args) {
+
+//        String location = "src/main/resources/USCensus1990.data.txt";
+        String location = "src/main/resources/sample.csv";
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 20_000);
+        props.put("linger.ms", 2);
+        props.put("buffer.memory", 35_000_000);
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<Integer, String> producer = new KafkaProducer<Integer, String>(props);
+
+
+        try (Stream<String> stream = Files.lines(Paths.get(location))) {
+
+            stream.skip(1).forEach(line -> {
+
+                        if (Integer.parseInt(line.split(",")[56]) == 0) {
+                            sendToKafka (line, TOPIC_MALE, producer);
+                        } else {
+                            sendToKafka (line, TOPIC_FEMALE, producer);
+                        }
+                    }
+            );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private static void sendToKafka (String line, String topic, Producer producer) {
+
+//        System.out.println(topic +":  " +  line + "\\n" );
+
+        ProducerRecord producerRecord = new ProducerRecord<Integer, String>(topic,
+                Integer.parseInt(line.split(",")[0]), line);
+        try {
+            producer.send(producerRecord).get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
